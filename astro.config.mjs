4a4e16f6da6a57ed3a2d30078ec6bfa46b6a9991
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-02-18 21:27:48 -0600

    Convert from Netlify to Node for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,18 @@ import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: netlify(),
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
   experimental: {
     fonts: [
       {
